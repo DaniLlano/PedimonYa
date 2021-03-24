506959494474e2dc050976dd472fcd15ab34e4ad
Daniela <danillano97@gmail.com>
2021-03-24 01:06:21 -0300

    cambios finales

--- index.js
+++ index.js
@@ -19,58 +19,53 @@ let productos = [
     },
 ];
 
+
 const $paVender = document.getElementById("paVender");
-const $carritoLista = document.getElementById("carritoLista")
-const $precioTotal = document.getElementById("precioTotal")
-const btnToggle = document.getElementById("btnToggle")
-let carrito = [];
+const $carritoLista = document.getElementById("carritoLista");
+const $precioTotal = document.getElementById("precioTotal");
+const btnToggle = document.getElementById("btnToggle");
 let carritoCantidad = 0;
+let carrito = [];
+
+document.addEventListener("click", agregarAlCarrito)
+document.addEventListener("click", borrarProducto)
 
 function pintarProductos(array) {
-    return($paVender.innerHTML = array.map((item) =>
+    $paVender.innerHTML = array.map((item) =>
     `<div class="pokemoncitos" id="${item.id}">
     <img src="${item.img}">
     <h3>${item.nombre}</h3>
     <h3>$${item.precio}</h3>
-    <button class="btncomprar">AGREGAR AL CARRITO</button></div>`
-    ));
+    <button id="btncomprar">AGREGAR AL CARRITO</button></div>`
+    ).join(" ");
 }
 
+function pintarProductosEnCarrito() {
+    $carritoLista.innerHTML = `<ul>${carrito.map((item) => `
+    <li>
+    <p><img src="${item.img}"></p>
+    <p>${item.nombre}</p>
+    <p>$${item.precio}</p>
+    <p id="${item.id}"><ion-icon name="trash" size="small" id="deleteBtn"></ion-icon></p>
+    </li>
+    `).join(" ")}
+    </ul>`;
+}
 
+function restarTotal() {
+    if (carrito.length >= 1) {
+        const carritoRestar = carrito.map((item) => item.precio);
+        const carritoRestaTotal = carritoRestar.reduce((accu, item) => accu + item);
+        $precioTotal.innerHTML = `
+        <p>TOTAL</p>
+        <p>$${carritoRestaTotal}</p>
+        <button id="Comprar">COMPRAR</button>
+        `;
+    }
+}
 
-
-function agregarAlCarrito(e) {
-    const productitoABuscar = e.target.parentNode.attributes.id.value;
-    const buscarProductito = productos.find((item) => item.id == productitoABuscar);
-    carrito.push(buscarProductito);
-    $carritoLista.innerHTML = `<table>
-    <thead>
-        <tr>
-        <td></td>
-        <td>Nombre</td>
-        <td>Precio</td>
-        <td></td>
-        </tr>
-    </thead>
-    <tbody class="miniContainer">${carrito.map((item) => `
-        <tr>
-        <th><img src="${item.img}"></th>
-        <td>${item.nombre}</td>
-        <td>$${item.precio}</td>
-        <td id="${item.id}"><ion-icon name="trash" size="small" class="deleteBtn"></ion-icon></td>
-        </tr>`).join(" ")}
-    </tbody>
-    </table>`;
-    $precioTotal.innerHTML = `
-    <p>Total</p>
-    <p>$${carrito[0].precio}</p>
-    <button id="Comprar">COMPRAR</button>
-    `;
-    carritoCantidad++;
-    btnToggle.innerHTML = `<ion-icon name="cart-sharp" size="large"></ion-icon>
-    <span class="numerito" id="cart-cantidad">${carritoCantidad}</span>
-    `
-    if (carrito.length > 1) {
+function sumarTotal() {
+    if (carrito.length >= 1) {
         const carritoSuma = carrito.map((item) => item.precio);
         const carritoTotal = carritoSuma.reduce((accu, item) => accu + item);
         $precioTotal.innerHTML = `
@@ -81,25 +76,33 @@ function agregarAlCarrito(e) {
     }
 }
 
+function agregarAlCarrito(e) {
+    if(e.target.id === "btncomprar") {
+        const productitoABuscar = e.target.parentNode.attributes.id.value;
+        const buscarProductito = productos.find((item) => item.id == productitoABuscar);
+        carrito.push(buscarProductito);
+        carritoCantidad++;
+        btnToggle.innerHTML = `<ion-icon name="cart-sharp" size="large"></ion-icon>
+        <span class="numerito" id="cart-cantidad">${carritoCantidad}</span>`;
+        sumarTotal(carrito);
+    }
+    pintarProductosEnCarrito(carrito);
+}
+
 function borrarProducto(e){
     if (e.target.id === "deleteBtn") {
         const paraBorrar = e.target.parentNode.attributes.id.value;
         const aEliminar = carrito.indexOf(paraBorrar);
-
         carrito.splice(aEliminar, 1);
-
-        
-    }
+        carritoCantidad--;
+        btnToggle.innerHTML = `<ion-icon name="cart-sharp" size="large"></ion-icon>
+        <span class="numerito" id="cart-cantidad">${carritoCantidad}</span>`;
+        restarTotal(carrito);
+    };
+    pintarProductosEnCarrito(carrito);
 }
 
-document.addEventListener("click", agregarAlCarrito)
-
-document.addEventListener("click", borrarProducto)
-
-
-
 pintarProductos(productos);
-
 
 
 
